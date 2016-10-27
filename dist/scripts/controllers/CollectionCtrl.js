305db136bc46da5b37afb23318e993708cc08dbe
coreybrown89 <coreybrown89@gmail.com>
2016-10-26 21:07:24 -0500

    assignment-6-services: completed

--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -1,10 +1,6 @@
 (function() {
 	function CollectionCtrl(Fixtures) {
-		this.albums = [];
-		for (var i=0; i < 12; i++) {
-			this.albums.push(angular.copy(albumPicasso));
-		}
-
+		this.albums = Fixtures.getCollection(12);
 	}
 
 	angular
--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -34,7 +34,18 @@
 
 		 Fixtures.getAlbum = function() {
 		 	return albumPicasso;
-		 }
+		 };
+
+		 Fixtures.getCollection = function(numberOfAlbums) {
+		 	var albums = [];
+
+		 	for (var i = 0; i < numberOfAlbums; i++) {
+		 		albums.push(angular.copy(albumPicasso));
+		 	}
+
+		 	return albums;
+
+		 };
 
 		return Fixtures;
 	}
